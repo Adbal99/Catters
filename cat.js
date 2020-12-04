ad556533fb51d654f5f38188554e13eb78261a7f
Adrian <adba1999@gmail.com>
2020-12-04 17:51:39 +0100

    Snall visual fixes

--- cat.js
+++ cat.js
@@ -1,22 +1,22 @@
-$(function() {
+$(function () {
     $("#combo-meter").hide();
     $("#pog").hide();
 
     //vars
-    var width =  $(document).width();
+    var width = $(window).width();
     var combo = 0;
     var disabled = false;
 
-    $("iframe").width( width );
+    $("iframe").width(width);
 
-    $("#get-cat").click(function(event) {
+    $("#get-cat").click(function (event) {
         $('#cat-source').attr("src", "https://media.tenor.com/images/01c4a5b287dbd39390d9256dda57c741/tenor.gif");
         let loading = true;
         if (loading) {
             $(this).html("Loading...")
             $(this).prop("disabled", true).delay(2000);
-            
-            $.get("https://api.thecatapi.com/v1/images/search", function(data) {
+
+            $.get("https://api.thecatapi.com/v1/images/search", function (data) {
                 $('#cat-source').attr("src", data[0].url);
                 combo++;
                 console.log(combo);
@@ -29,22 +29,22 @@ $(function() {
 
         }
         $(this).prop("disabled", false);
-        if(combo == 9){
+        if (combo == 9) {
             $("#pog").show();
         }
     })
-    
-    $("#zacek").click(function(){
+
+    $("#zacek").click(function () {
         console.log("click")
         $(this).removeClass("text-yellow-400");
     })
-    $("#hide-img").click(function(){
-        if(!disabled){
+    $("#hide-img").click(function () {
+        if (!disabled) {
             disabled = true;
             $("#cat-img").hide();
             $("#get-cat").prop("disabled", true);
             $(this).html("Bring back my cats!");
-        }else{
+        } else {
             $("#cat-img").show();
             disabled = false;
             $(this).html("Hide Img & Vibe B)");
